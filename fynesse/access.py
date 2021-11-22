8b1097f6fd88e6aabc22dbd23990aeba57707373
Karl Mose <karl.h.mose@gmail.com>
2021-11-22 16:33:41 +0000

    1

--- fynesse/access.py
+++ fynesse/access.py
@@ -21,6 +21,10 @@ def data(credentials, database_details):
                              host=database_details["url"],
                              database="property_prices")
 
+    ## Creates the schemas
+    create_pp_data_table(conn)
+    create_postcode_table(conn)
+
     ## Uploads the data to the SQL server
     load_pp_to_sql(conn)
     load_postcode_to_sql(conn)
@@ -76,3 +80,86 @@ def load_postcode_to_sql(conn):
     conn.cursor().execute(query)
     conn.commit()
 
+
+def create_pp_data_table(conn):
+    query = """"
+    SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+    SET time_zone = "+00:00";
+
+    CREATE DATABASE IF NOT EXISTS `property_prices` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+    USE `property_prices`;
+
+    --
+    -- Table structure for table `pp_data`
+    --
+    DROP TABLE IF EXISTS `pp_data`;
+    CREATE TABLE IF NOT EXISTS `pp_data` (
+      `transaction_unique_identifier` tinytext COLLATE utf8_bin NOT NULL,
+      `price` int(10) unsigned NOT NULL,
+      `date_of_transfer` date NOT NULL,
+      `postcode` varchar(8) COLLATE utf8_bin NOT NULL,
+      `property_type` varchar(1) COLLATE utf8_bin NOT NULL,
+      `new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,
+      `tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,
+      `primary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
+      `secondary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
+      `street` tinytext COLLATE utf8_bin NOT NULL,
+      `locality` tinytext COLLATE utf8_bin NOT NULL,
+      `town_city` tinytext COLLATE utf8_bin NOT NULL,
+      `district` tinytext COLLATE utf8_bin NOT NULL,
+      `county` tinytext COLLATE utf8_bin NOT NULL,
+      `ppd_category_type` varchar(2) COLLATE utf8_bin NOT NULL,
+      `record_status` varchar(2) COLLATE utf8_bin NOT NULL,
+      `db_id` bigint(20) unsigned NOT NULL
+    ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+    --
+    -- Indexes for table `pp_data`
+    --
+    ALTER TABLE `pp_data`
+    ADD PRIMARY KEY (`db_id`);
+    MODIFY `db_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+    CREATE INDEX `pp.postcode` USING HASH
+      ON `pp_data`
+        (postcode);
+    CREATE INDEX `pp.date` USING HASH
+      ON `pp_data` 
+        (date_of_transfer);"""
+    conn.cursor().execute(query)
+    conn.commit()
+
+def create_postcode_table(conn):
+    query = """"USE `property_prices`;
+        --
+        -- Table structure for table `postcode_data`
+        --
+        DROP TABLE IF EXISTS `postcode_data`;
+        CREATE TABLE IF NOT EXISTS `postcode_data` (
+          `postcode` varchar(8) COLLATE utf8_bin NOT NULL,
+          `status` enum('live','terminated') NOT NULL,
+          `usertype` enum('small', 'large') NOT NULL,
+          `easting` int unsigned,
+          `northing` int unsigned,
+          `positional_quality_indicator` int NOT NULL,
+          `country` enum('England', 'Wales', 'Scotland', 'Northern Ireland', 'Channel Islands', 'Isle of Man') NOT NULL,
+          `lattitude` decimal(11,8) NOT NULL,
+          `longitude` decimal(10,8) NOT NULL,
+          `postcode_no_space` tinytext COLLATE utf8_bin NOT NULL,
+          `postcode_fixed_width_seven` varchar(7) COLLATE utf8_bin NOT NULL,
+          `postcode_fixed_width_eight` varchar(8) COLLATE utf8_bin NOT NULL,
+          `postcode_area` varchar(2) COLLATE utf8_bin NOT NULL,
+          `postcode_district` varchar(4) COLLATE utf8_bin NOT NULL,
+          `postcode_sector` varchar(6) COLLATE utf8_bin NOT NULL,
+          `outcode` varchar(4) COLLATE utf8_bin NOT NULL,
+          `incode` varchar(3)  COLLATE utf8_bin NOT NULL,
+          `db_id` bigint(20) unsigned NOT NULL
+        ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+        
+        ALTER TABLE `postcode_data`
+        ADD PRIMARY KEY (`db_id`);
+        MODIFY `db_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+        CREATE INDEX `po.postcode` USING HASH
+          ON `postcode_data`
+            (postcode);"""
+    conn.cursor().execute(query)
+    conn.commit()
